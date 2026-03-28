14573968636922705e7336892909d04ace5e5138
dimitriost1 <46889040+dimitriost1@users.noreply.github.com>
2026-03-28 21:04:26 +0200

    feat: Setup MUI and Roboto font.

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,7 @@
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,6 +3,7 @@ import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
 
 import '../styles.css'
+import { CssBaseline } from '@mui/material'
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -11,6 +12,7 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <>
+      <CssBaseline />
       <Outlet />
       <TanStackDevtools
         config={{
